ebbbcc3d0b520705e1dc741b4e37256fd3c88b00
Alberto Chervenkov <albertotch@abv.bg>
2023-03-21 15:14:39 +0200

    04.1 DOMAndEvents Lab Commit

--- 04.1 DOMAndEvents Lab/03. Collect-List-Items/CollectListItems.js
+++ 04.1 DOMAndEvents Lab/03. Collect-List-Items/CollectListItems.js
@@ -1,3 +1,11 @@
 function extractText() {
-    // TODO
+    const elements=document.getElementsByTagName("li");
+
+    let text="";
+    for (let i = 0; i < elements.length; i++) {
+        text += elements[i].innerHTML + "\n";
+    }
+    const resultTextArea=document.getElementById('result');
+
+    resultTextArea.value=text;
 }
--- 04.1 DOMAndEvents Lab/05. Delete-From-Table/app.js
+++ 04.1 DOMAndEvents Lab/05. Delete-From-Table/app.js
@@ -1,3 +1,15 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const result=document.getElementById('result');
+    let inputValue=document.querySelector('input[name="email"]').value;
+    const evenTds=Array.from(document.querySelectorAll('td:nth-child(even)'));
+
+    let foundElement=evenTds.find((td)=> td.textContent === inputValue);
+
+    if (foundElement) {
+        result.textContent='Deleted';
+        foundElement.parentElement.remove();
+    }
+    else{
+        result.textContent='Not found.';
+    }
 }
