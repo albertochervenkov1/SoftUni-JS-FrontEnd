ce58e36618d99d6bd0cbacb2b4551749982fb10e
Alberto Chervenkov <albertotch@abv.bg>
2023-03-24 18:05:01 +0200

    05.2 HTTPAndREST Exercise Commit

--- 05.2 HTTPAndREST Exercise/01.Bus-Stop/app.js
+++ 05.2 HTTPAndREST Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,24 @@
 function getInfo() {
-    // TODO:
+    const stopId = document.querySelector('#stopId')
+    const stopName = document.querySelector('#stopName')
+    const buses = document.querySelector('#buses')
+
+
+    function createElementWithTextContent(tag, textContent) {
+        const e = document.createElement(tag)
+        e.textContent = textContent
+        return e
+    }
+
+    buses.innerHTML='';
+    const busInformation = 'http://localhost:3030/jsonstore/bus/businfo/'
+    fetch(`${busInformation}${stopId.value}`).then(x => x.json()).then(x => {
+        stopName.textContent = x['name']
+        for (const key in x['buses']) {
+            buses.appendChild(createElementWithTextContent('li', `Bus ${key} arrives in ${x['buses'][key]} minutes`))
+        }
+    }).catch(() => {
+        stopName.textContent = 'Error'
+    })
+
 }
--- 05.2 HTTPAndREST Exercise/06.Phonebook/app.js
+++ 05.2 HTTPAndREST Exercise/06.Phonebook/app.js
@@ -1,5 +1,64 @@
 function attachEvents() {
-    // TODO:
+    const phonebookContainer=document.getElementById('phonebook');
+    const personInput=document.getElementById('person');
+    const phoneInput=document.getElementById('phone');
+    const loadBtn=document.getElementById('btnLoad');
+    const createBtn=document.getElementById('btnCreate');
+    const BASE_URL='http://localhost:3030/jsonstore/phonebook/';
+
+    loadBtn.addEventListener('click', loadPhonebookHandler);
+    createBtn.addEventListener('click', createPhoneBookHandler)
+
+    async function loadPhonebookHandler(){
+        const phoneBookRes=await fetch(BASE_URL);
+        const phonebookData=await phoneBookRes.json();
+        const values=Object.values(phonebookData);
+        phonebookContainer.innerHTML='';
+        for (const {phone, person, _id} of values) {
+            const li=document.createElement('li');
+
+            const button=document.createElement('button');
+            button.textContent='Delete';
+            button.id=_id;
+            button.addEventListener('click', deletePhonebookHandler);
+
+            li.innerHTML=`${person}: ${phone}`;
+            li.appendChild(button);
+            phonebookContainer.appendChild(li);
+        }
+    }
+
+    async function createPhoneBookHandler(){
+        const person=personInput.value;
+        const phone=phoneInput.value;
+        const httpHeaders={
+            method: 'POST',
+            body: JSON.stringify({person, phone})
+        }
+        fetch(BASE_URL,httpHeaders)
+        .then((res) => res.json)
+        .then(()=>{
+            loadPhonebookHandler();
+            personInput.value='';
+            phoneInput.value='';
+        })
+        .catch((err)=> {
+            console.error(err);
+        })
+    }
+    
+    async function deletePhonebookHandler(){
+        const id=this.id;
+        const httpHeaders={
+            method:'DELETE'
+        };
+        fetch(`${BASE_URL}${id}`,httpHeaders)
+        .then((res)=> res.json)
+        .then(loadPhonebookHandler)
+        .catch((err)=>{
+            console.error(err);
+        })
+    }
 }
 
 attachEvents();
