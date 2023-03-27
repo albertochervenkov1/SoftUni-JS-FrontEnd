499e6a7f07ffaa2c4ee021ca401384f384b075eb
Alberto Chervenkov <albertotch@abv.bg>
2023-03-27 16:20:41 +0300

    05.2 HTTPAndREST Exercise Commit

--- 05.2 HTTPAndREST Exercise/04.Blog/app.js
+++ 05.2 HTTPAndREST Exercise/04.Blog/app.js
@@ -1,60 +1,63 @@
 function attachEvents() {
-    const btnLoadPosts=document.getElementById('btnLoadPosts');
-    const btnViewPost=document.getElementById('btnViewPost');
-    const select=document.getElementById('posts');
-    const selector = document.getElementById('posts');
-    const POST_URL='http://localhost:3030/jsonstore/blog/posts';
-    const COMMENT_URL='http://localhost:3030/jsonstore/blog/comments';
+    const [btnLoadPosts, btnViewPost] = Array.from(document.querySelectorAll('button'))
+    const posts = document.querySelector('#posts')
+    const postTitle = document.querySelector('#post-title')
+    const postBody = document.querySelector('#post-body')
+    
+    let postInfo = {}
+    const postsApiUrl = 'http://localhost:3030/jsonstore/blog/posts'
+    const commentsApiUrl = 'http://localhost:3030/jsonstore/blog/comments'
 
-    btnLoadPosts.addEventListener('click', getPosts);
-    btnViewPost.addEventListener('click', getComments);
-
-    async function getPosts(){
-        const postsRes=await fetch(POST_URL);
-        const postsData=await postsRes.json();
-        const values=Object.values(postsData);
-        console.log(values);
-        for (const {body, id, title} of values) {
-            const newOption=document.createElement('option');
-            newOption.textContent=title.toUpperCase();
-            newOption.value=id;
-            select.appendChild(newOption);
+    function createElementWithValueAndTextContent(tag, value, textContent) {
+        const e = document.createElement(tag)
+        e.value = value
+        e.textContent = textContent
+        return e
+    }
+    
+    function findPost(selected) {
+        for (const key in postInfo) {
+            if (selected === postInfo[key].id) {
+                return postInfo[key]
+            }
         }
     }
+    
+    
+    btnLoadPosts.addEventListener('click', () => {
+        fetch(postsApiUrl).then(x => x.json()).then(x => {
+            postInfo = x
+            for (const key in x) {
+                posts.appendChild(createElementWithValueAndTextContent('option', x[key].id, x[key].title))
+            }
+        }).catch()
+    })
 
-    async function getComments(){
-        const selectedPostID = selector.value;
-        const postTitle = document.getElementById('post-title');
-        const postBody = document.getElementById('post-body');
-        const postsContainer = document.getElementById('post-comments');
-        postsContainer.innerHTML = '';
-        postTitle.textContent='';
-        postBody.textContent='';
-
-        const postRes=await fetch(`${POST_URL}/${selectedPostID}`);
-        const postData=await postRes.json();
-        console.log(postData);
-        postTitle.textContent=postData.title;
-        postBody.textContent=postData.body;
-        console.log(postTitle.textContent);
-        console.log(postBody.textContent);
-
-        fetch(COMMENT_URL)
-            .then((res)=> res.json())
-            .then((comments) => {
-                for (const id in comments){
-                    const currentComment = comments[id];
-                    if (currentComment.postId===selectedPostID){
-                        const commentLi = document.createElement('li');
-                        commentLi.id = currentComment.id;
-                        commentLi.textContent = currentComment.text;
-                        postsContainer.appendChild(commentLi);
-                    }
+    btnViewPost.addEventListener('click', () => {
+        fetch(commentsApiUrl).then(x => x.json()).then(x => {
+            let post = findPost(posts.value)
+            postTitle.textContent = post.title
+            postBody.textContent = post.body
+            const boby = document.querySelector('body')
+            const ul = document.querySelector('#post-comments')
+            
+            boby.removeChild(ul)
+            
+            const newUl = document.createElement('ul')
+            newUl.setAttribute('id', 'post-comments')
+            boby.appendChild(newUl)
+            const postComments = document.querySelector('#post-comments')
+            
+            for (const key in x) {
+                if (x[key].postId === posts.value) {
+                    const l = document.createElement('li')
+                    l.textContent = x[key].text
+                    postComments.appendChild(l)
                 }
-            })
-            .catch((err) => console.log(err))
-        
-    }
+            }
+        }).catch()
+    })
+
 }
 
 attachEvents();
